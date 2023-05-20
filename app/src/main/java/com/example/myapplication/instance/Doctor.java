9e62e93a0ad029da3b22cb3fa50334d926671da5
TuanAnh <anhdt.dsai02@outlook.com.vn>
2023-05-20 12:30:28 +0700

    create Recycle View, button for IT user

---
+++ app/src/main/java/com/example/myapplication/instance/Doctor.java
@@ -0,0 +1,18 @@
+package com.example.myapplication.instance;
+
+public class Doctor {
+    private String name;
+
+    public Doctor(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+
+    }
+}
