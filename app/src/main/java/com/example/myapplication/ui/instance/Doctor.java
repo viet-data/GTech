5087ecdc9494d0fe123925e673309d5095b0d337
TuanAnh <anhdt.dsai02@outlook.com.vn>
2023-06-03 14:44:42 +0700

    create Recycle View, connect with fireStore which contain list of doctors

---
+++ app/src/main/java/com/example/myapplication/ui/instance/Doctor.java
@@ -0,0 +1,18 @@
+package com.example.myapplication.ui.instance;
+
+public class Doctor {
+    String name;
+    public Doctor(){}
+
+    public Doctor(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
