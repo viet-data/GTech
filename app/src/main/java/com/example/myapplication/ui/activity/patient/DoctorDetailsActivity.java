84d0d0713d6e2d17fe63cea11f67e803ffff67d3
TuanAnh <anhdt.dsai02@outlook.com.vn>
2023-07-15 10:24:11 +0700

    back toolbar

--- app/src/main/java/com/example/myapplication/ui/activity/patient/DoctorDetailsActivity.java
+++ app/src/main/java/com/example/myapplication/ui/activity/patient/DoctorDetailsActivity.java
@@ -1,10 +1,12 @@
 package com.example.myapplication.ui.activity.patient;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.example.myapplication.R;
@@ -14,11 +16,20 @@ import com.example.myapplication.model.Doctor;
 import org.w3c.dom.Text;
 
 public class DoctorDetailsActivity extends AppCompatActivity {
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_details);
+
+        toolbar = findViewById(R.id.back_toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
 
         Bundle bundle = getIntent().getExtras();
         Doctor doctor = bundle.getParcelable("doctor");
